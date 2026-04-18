d3d534dc84e78f40aa3bd661e250d3613bb06b3c
Murilo <mbalves1@outlook.com>
2026-04-18 09:16:11 -0300

    feat: integrate WalletRepository to manage account balance checks in PlaceOrder

--- backend/src/application/usecase/PlaceOrder.ts
+++ backend/src/application/usecase/PlaceOrder.ts
@@ -4,6 +4,7 @@ import OrderRepository from '../../infra/repository/OrderRepository';
 import Order from '../../domain/Order';
 import ExecuteOrder from './ExecuteOrder';
 import Mediator from '../../infra/mediator/Mediator';
+import WalletRepository from '../../infra/repository/WalletRepository';
 
 export default class PlaceOrder {
 	@inject('accountRepository')
@@ -12,8 +13,17 @@ export default class PlaceOrder {
 	orderRepository!: OrderRepository;
 	@inject('mediator')
 	mediator!: Mediator;
+	@inject('walletRepository')
+	walletRepository!: WalletRepository;
 
 	async execute(input: Input): Promise<Output> {
+		const wallet = await this.walletRepository.getByAccountId(input.accountId);
+		const [mainAsset, paymentAsset] = input.marketId.split('-');
+		const asset = input.side === 'buy' ? paymentAsset : mainAsset;
+		const balance = wallet.getBalance(asset);
+		if (!balance || balance.quantity < input.quantity)
+			throw new Error('Insufficient funds');
+
 		const order = Order.create(
 			input.accountId,
 			input.marketId,

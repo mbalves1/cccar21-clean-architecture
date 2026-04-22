412ba588c3a498cc2abd67f4390d5b4b53daf4a2
Murilo <mbalves1@outlook.com>
2026-04-22 08:29:02 -0300

    feat: WIP - enhance ORM save method to construct SQL insert query and improve logging

--- backend/src/infra/orm/ORM.ts
+++ backend/src/infra/orm/ORM.ts
@@ -1,6 +1,9 @@
 export default class ORM {
 	async save(model: Model) {
-		console.log('Model', model);
+		console.log('Model', model.schema, model.table, model.columns);
+		const columns = model.columns.map((column: any) => column.column).join(',');
+		const params = model.columns.map((_, index) => `$${index + 1}`).join(',');
+		const query = `insert into ${model.schema}.${model.table} (${columns}) values (${params})`;
 	}
 
 	async get(model: Model, field: string, value: string): Promise<any> {
@@ -11,15 +14,21 @@ export default class ORM {
 class Model {
 	schema!: string;
 	table!: string;
-	column!: { column: string; property: string };
+	columns!: { column: string; property: string }[];
 }
 
+@model('cccar', 'account')
 export class AccountModel extends Model {
-	accountId: string;
-	name: string;
-	email: string;
-	document: string;
-	password: string;
+	@column('account_id')
+	accountId!: string;
+	@column('name')
+	name!: string;
+	@column('email')
+	email!: string;
+	@column('document')
+	document!: string;
+	@column('password')
+	password!: string;
 
 	constructor(
 		accountId: string,
@@ -35,4 +44,18 @@ export class AccountModel extends Model {
 		this.document = document;
 		this.password = password;
 	}
+}
+
+function model(schema: string, table: string) {
+	return function (target: any) {
+		target.prototype.schema = schema;
+		target.prototype.table = table;
+	};
+}
+
+function column(column: string) {
+	return function (target: any, propertyKey: string) {
+		target.columns = target.columns || [];
+		target.columns.push({ column, property: propertyKey });
+	};
 }
